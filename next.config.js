67fdfab6aa92921d43c530421224a51885639218
bulhwi <mm225012@gmail.com>
2023-12-21 01:55:48 +0900

    basePath 수정

--- next.config.js
+++ next.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
-    basePath: 'yjystyle.github.io',
+    basePath: '/page',
     images: {
       unoptimized: true,
       remotePatterns: [
