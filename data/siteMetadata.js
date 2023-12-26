673ed7dfb5fa5f42fc286e8acda7dd21e791204b
여준영 <yeogandalf@gmail.com>
2023-12-26 16:42:16 +0900

    레이아웃 변경

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -61,7 +61,7 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
@@ -69,7 +69,7 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: 'ko',
     },
   },
   search: {
